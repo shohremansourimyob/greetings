a278eb165f734e9242dc10c80ba65b679731cb6b
Shohre Mansouri <shohre.mansouri@myob.com>
2023-03-07 15:00:12 +1100

    Added Unknown greeting

--- src/greet.js
+++ src/greet.js
@@ -1,13 +1,25 @@
 import hello from "./hello";
 import pleasantry from "./pleasantry";
 
-const greet = () => {
+const normalGreeting = () => {
     const helloStateent = hello();
 
     const greetStatement = helloStateent;
     const pleasantryStatement = pleasantry();
 
     return `${greetStatement}! ${pleasantryStatement}`;
+}
+
+const unknownGreeting = () => {
+     return 'I beg your pardon?';
+} 
+
+const greet = (inputGreet) => {
+    if (inputGreet === 'Hello!') {
+        return normalGreeting();
+    }
+
+    return unknownGreeting();
 }
 
 export default greet;
--- src/greet.test.js
+++ src/greet.test.js
@@ -3,7 +3,15 @@ import greet from "./greet";
 it("should say Hello! How are you?, when someone says Hello", () => {
     const expected = 'Hi! How are you?';
 
-    const response = greet();
+    const response = greet("Hello!");
+
+    expect(response).toBe(expected);
+});
+
+it('should say I beg your pardon, when an unknown word is provided', () => {
+    const expected = 'I beg your pardon?';
+
+    const response = greet('blah blah');
 
     expect(response).toBe(expected);
 });
